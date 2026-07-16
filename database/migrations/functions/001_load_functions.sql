fc8f950b0f60a20b2375333a7d20910af6a07741
Shahriyar Mammadov <sako@Sako.local>
2026-07-15 23:22:18 -0400

    fix: load intentional dating database functions

--- database/migrations/functions/001_load_functions.sql
+++ database/migrations/functions/001_load_functions.sql
@@ -80,6 +80,8 @@
 {{ template "alliance/update_alliance_views.sql" }}
 
 {{ template "dashboard-common/search_user.sql" }}
+{{ template "dashboard-common/add_intentional_dating_intro.sql" }}
+{{ template "dashboard-common/list_intentional_dating_opt_ins.sql" }}
 {{ template "dashboard-common/update_group.sql" }}
 
 {{ template "dashboard-alliance/activate_group.sql" }}
